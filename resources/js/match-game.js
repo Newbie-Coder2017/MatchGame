82ee00eca3cfbc46a4471fafac46e1fcc826bced
Newbie-Coder2017 <Newbie-Coder2017@users.noreply.github.com>
2017-07-01 15:25:12 -0400

    removed console.logs

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -47,13 +47,12 @@ MatchGame.renderCards = function(cardValues, $game) {
     $card.data('flipped', false);
     $card.data('color', cardColors[value - 1]);
     $game.append($card);
-    print($card);
   }
 
   // LIstener for when a card is clicked
   $('.card').on('click', (function() {
-    $card = (this);
     MatchGame.flipCard($card, $game);
+
   }))
 }
 
@@ -64,39 +63,33 @@ MatchGame.renderCards = function(cardValues, $game) {
 
 MatchGame.flipCard = function($card, $game) {
   var flippedCards = [];
-  // $game.data('revealed', flippedCards);
+  $game.data('revealed', flippedCards);
 
   // Flip the Card if the card has not already been flipped
 
 
   if ($card.data('flipped') !== true) {
     // Check is the card is face down, and show the characteristics of the flipped card.
+    flippedCards.push($card.data('value'));
     $card.css('background-color', $card.data('color'));
     $card.text($card.data('value'));
     $card.data('flipped', true);
-    flippedCards.push($card);
   } else {
     // The card has already been flipped, do nothing to that card.
-    return
-  }
-
-  // Check if the game has 2 flipped cards
-
-  if (flippedCards.length === 2) {
-    var value1 = flippedCards[0];
-    var value2 = flippedCards[1];
-    if (value1 === value2) {
-      $card.css('background-color', 'rgb(153,153,153)');
-      $card.css('color', 'rgb(204,204,204)');
-      flippedCards.empty();
+    if (flippedCards.length === 2) {
+        // Check if the game has 2 flipped cards
+      if (flippedCards[0] === flippedCards[1]) {
+        $card.css('background-color', 'rgb(153,153,153)');
+        $card.css('color', 'rgb(204,204,204)');
+      } else {
+        $card.css('background-color', 'rgb(32, 64, 86)');
+        $card.text(' ');
+        $card.data('flipped', false);
+      }
+      $game.data('revealed', []);
     } else {
-      $card.css('background-color', 'rgb(32, 64, 86)');
-      $card.text(' ');
-      $card.data('flipped', false);
-      flippedCards.empty();
-    }
-  } else {
 
+    }
   }
 
 }
